8c577c42803ab35d5d00ea3e76bd182cf0db5014
gustavocestero <gcestero456@gmail.com>
2024-10-02 16:07:08 -0400

    all components integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,15 @@ import {AboutComponent} from "./components/about/about.component";
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent,ProjectsComponent, ProfileComponent, HeaderComponent,],
+  imports: [RouterOutlet, AboutComponent,ProjectsComponent, ProfileComponent, HeaderComponent, ExperienceComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent {
+
+  goToLinkedin() {
+    window.open('https://www.linkedin.com/in/gustavo-cestero-181b3b196', '_blank');
+  }
+
+  get email(): string {
+    return 'gcestero456@gmail.com';
+  }
+}
