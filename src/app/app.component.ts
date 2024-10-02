b7a40cce177ffed404fc630d84189c686165fd4b
gustavocestero <gcestero456@gmail.com>
2024-10-02 13:18:28 -0400

    Deleted "experioence"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,12 @@ import {AboutComponent} from "./components/about/about.component";
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ExperienceComponent } from './components/experience/experience.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent,ProjectsComponent, ProfileComponent, HeaderComponent, ExperienceComponent],
+  imports: [RouterOutlet, AboutComponent,ProjectsComponent, ProfileComponent, HeaderComponent,],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
